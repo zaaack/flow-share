76f3dfb5f36791b6445bacb550afae8822d73667
zaaack <zaaack@qq.com>
2017-08-31 22:53:43 +0800

    Init

---
+++ test.js
@@ -0,0 +1,40 @@
+  // @flow
+
+
+let logger = ['log', 'error', 'warn', 'debug', 'info']
+  .reduce((logger, name) => {
+    logger[name] = console[name].bind(console, 'MyApp')
+    return logger
+  }, {})
+
+logger.log("aa", "bb", 123)
+
+
+class A {
+  static aa: number
+}
+
+
+class B extends A {
+  static bb: string
+}
+
+
+type Action =
+  | {| type: "FETCH_PENDING", meta: {id: string} |}
+  | {| type: "FETCH_SUCCESS", payload: { title: string } |}
+  | {| type: "FETCH_FAILURE", error: { message: string } |}
+
+function reducer(state={}, action: Action) {
+	switch (action.type) {
+		case 'FETCH_PENDING':
+      action.meta.id
+			//...
+			return state
+		case 'FETCH_FAILURE':
+      console.log(action.error.message)
+			//...
+			return state
+	}
+	return state
+}
